d7d9ca3577cd5376738639acbb0061215f57f4d5
Karthik Ramachandran <karthikr1493@gmail.com>
2022-07-17 17:44:46 +0530

    Configured Pose detection which uses camera.

--- app/src/main/java/com/karthikr/posedetection/MainActivity.kt
+++ app/src/main/java/com/karthikr/posedetection/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.karthikr.posedetection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.mlkit.vision.pose.PoseDetector
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
+import com.karthikr.posedetection.vision.posedetector.PoseDetectorProcessor
+import com.karthikr.posedetection.helper.MLVideoHelperActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MLVideoHelperActivity() {
+    lateinit var poseDetector: PoseDetector
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    override fun setProcessor() {
+        val options = AccuratePoseDetectorOptions.Builder()
+            .setDetectorMode(AccuratePoseDetectorOptions.STREAM_MODE)
+            .build()
+        cameraSource?.setMachineLearningFrameProcessor(
+            PoseDetectorProcessor(
+                this,
+                options,
+                true,
+                false,
+                false,
+                false,
+                true
+            )
+        )
     }
 }
